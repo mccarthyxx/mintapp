e418265960ebca20c7d14cbde411e3d720122cd1
mccarthyxyz <belliblobstudios@gmail.com>
2022-05-09 19:48:40 +0800

    last

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,5 +1,5 @@
-const contractAddress = ""; //Replace with your own contract address
-const chain = 'rinkeby'; // rinkeby or polygon
+const contractAddress = "0x0e724Cf24EF7CAB8d414E6dc362c72Feb7b6aFbF"; //Replace with your own contract address
+const chain = 'polygon'; // rinkeby or polygon
 
 const welcome_h1 = "Welcome to the Partybear.io NFT Project!!";
 const welcome_h2 = "Connect to MetaMask to Get Started";
